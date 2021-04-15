8e4a7b99081022fb3c81c69c4ddb33b117bc0ff0
Frank-Landry <franklandry85@gmail.com>
2021-04-14 21:59:32 -0400

    Fixing need for manual toggle in mobile view

--- resources/js/sidemenu.js
+++ resources/js/sidemenu.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuIcons = document.getElementsByClassName('hamburger');
-
+    const sideMenu = document.getElementById('sideMenu');
     if (!menuIcons) {
         return;
+    }
+
+    if (!menuIcons[0].classList.contains('hamburger-enabled')) {
+        sideMenu.classList.toggle('hidden');
+        sideMenu.classList.toggle('sm:flex');
+        sideMenu.classList.toggle('bg-gray-700');
     }
 
     Object.values(menuIcons).forEach(icon => {
@@ -11,8 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-function toggleMenu()
-{
+function toggleMenu() {
     const sideMenu = document.getElementById('sideMenu');
     if (!sideMenu) {
         return;
