4ecffece8f208e642515006cce84fbfeaec067c5
Frank Landry <51010488+Frank-Landry@users.noreply.github.com>
2021-04-15 15:04:44 -0400

    Fixing mobile nav transition to auto hide menu on page load (#4)
    * Created new js file to help nav toggle. Added classes to accomodate for mobile view. Added icon for mobile view only
    
    * Add sidemenu to app.js instead of loading separately
    Add public/css public/js to gitignore(server compiles on deploy)
    
    Signed-off-by: Sam Parton <tony8tr@hotmail.com>
    
    * Replace menu icon with hero icon(just so we're not using a whole icon lib for 1 icon :D)
    Use hamburger-enabled class when menu is toggled so a couple more styles can be applied
    Move menu toggle to top left of logo section
    Make menu scrollable when open(if content goes off screen)
    
    Signed-off-by: Sam Parton <sam@idevelopthings.com>
    
    * Add bar at top of the page with "envuso" and hamburger menu icon
    Adjust margin on x-title component
    Change the sidebar header with image to a component
    Change sidebar js a tad, was to multiple toggle icons... which isn't necessary so much anymore... but cba to change back
    Move some styling to app.css
    
    Signed-off-by: Sam Parton <sam@idevelopthings.com>
    
    * Fixing need for manual toggle in mobile view
    
    Co-authored-by: Sam Parton <sam@idevelopthings.com>

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -18,55 +18,54 @@
 <body class="antialiased min-h-screen bg-gray-900">
 
 
-<x-mobile-menu-header/>
+    <x-mobile-menu-header />
 
-<div class="body-wrapper">
-
-
-    <div id="sideMenu" class="sidebar ">
-
-        <x-sidebar-header/>
-
-        <div class="px-6 py-5">
-
-            <x-sidebar-item :route="route('overview.setup')" text="Getting Started" />
-
-            <x-sidebar-item-group title="Prologue">
-                <x-sidebar-group-item :route="route('overview.release-notes')" text="Release Notes" />
-                <x-sidebar-group-item :route="route('overview.contribute')" text="Contribute" />
-            </x-sidebar-item-group>
-
-            <x-sidebar-item-group title="HTTP">
-                <x-sidebar-group-item :route="route('overview.routes')" text="Routes" />
-                <x-sidebar-group-item :route="route('overview.request')" text="Request" />
-                <x-sidebar-group-item :route="route('overview.response')" text="Response" />
-                <x-sidebar-group-item :route="route('overview.controllers')" text="Controllers" />
-                <x-sidebar-group-item :route="route('overview.middleware')" text="Middleware" />
-            </x-sidebar-item-group>
-
-            <x-sidebar-item-group title="Database">
-                <x-sidebar-group-item :route="route('overview.models')" text="Models" />
-                <x-sidebar-group-item :route="route('overview.query-builder')" text="Query builder" />
-            </x-sidebar-item-group>
-
-            <x-sidebar-item-group title="Database">
-                <x-sidebar-group-item :route="route('overview.authentication')" text="Authentication" />
-            </x-sidebar-item-group>
-
-            <x-sidebar-item :route="route('overview.cli')" text="CLI" />
-            <x-sidebar-item :route="route('overview.decorators')" text="Decorators" />
+    <div class="body-wrapper">
 
 
+        <div id="sideMenu" class="sidebar ">
+
+            <x-sidebar-header />
+
+            <div class="px-6 py-5">
+
+                <x-sidebar-item :route="route('overview.setup')" text="Getting Started" />
+
+                <x-sidebar-item-group title="Prologue">
+                    <x-sidebar-group-item :route="route('overview.release-notes')" text="Release Notes" />
+                    <x-sidebar-group-item :route="route('overview.contribute')" text="Contribute" />
+                </x-sidebar-item-group>
+
+                <x-sidebar-item-group title="HTTP">
+                    <x-sidebar-group-item :route="route('overview.routes')" text="Routes" />
+                    <x-sidebar-group-item :route="route('overview.request')" text="Request" />
+                    <x-sidebar-group-item :route="route('overview.response')" text="Response" />
+                    <x-sidebar-group-item :route="route('overview.controllers')" text="Controllers" />
+                    <x-sidebar-group-item :route="route('overview.middleware')" text="Middleware" />
+                </x-sidebar-item-group>
+
+                <x-sidebar-item-group title="Database">
+                    <x-sidebar-group-item :route="route('overview.models')" text="Models" />
+                    <x-sidebar-group-item :route="route('overview.query-builder')" text="Query builder" />
+                </x-sidebar-item-group>
+
+                <x-sidebar-item-group title="Database">
+                    <x-sidebar-group-item :route="route('overview.authentication')" text="Authentication" />
+                </x-sidebar-item-group>
+
+                <x-sidebar-item :route="route('overview.cli')" text="CLI" />
+                <x-sidebar-item :route="route('overview.decorators')" text="Decorators" />
+
+            </div>
         </div>
+
+        <div class="content">
+            @yield('content')
+        </div>
+
     </div>
 
-    <div class="content">
-        @yield('content')
-    </div>
-
-</div>
-
-<script src="{{mix('js/app.js')}}"></script>
+    <script src="{{mix('js/app.js')}}"></script>
 
 </body>
 
