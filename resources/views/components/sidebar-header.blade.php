60b4fe2354771ff9f81e2a901dff867ca7809376
Frank-Landry <franklandry85@gmail.com>
2021-04-26 21:55:47 -0400

    adding alt attribute to img tag

--- resources/views/components/sidebar-header.blade.php
+++ resources/views/components/sidebar-header.blade.php
@@ -1,3 +1,4 @@
-<div class="relative bg-gradient-to-t to-indigo-900 from-gray-900 p-8 flex items-center justify-center border-b border-gray-700">
-    <img src="/assets/logo.svg" />
+<div
+    class="relative bg-gradient-to-t to-indigo-900 from-gray-900 p-8 flex items-center justify-center border-b border-gray-700">
+    <img src="/assets/logo.svg" alt="sidebar mountain logo for envuso" />
 </div>
